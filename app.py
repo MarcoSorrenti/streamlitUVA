18b180a818487eaa99a4cf32750b46399ba5a798
Marco <marcosorrenti93@gmail.com>
2025-11-24 20:34:31 +0100

    test

--- app.py
+++ app.py
@@ -150,7 +150,14 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # ============================================================
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("logo.png", width=65)   # LOGO MOLTO PIÙ PICCOLO
+    st.markdown(
+        """
+        <div style="display: flex; justify-content: center;">
+            <img src="logo.png" width="65">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 st.markdown("<div class='title-text'>Smart Harvesting</div>", unsafe_allow_html=True)
 st.markdown("<div class='subtitle-text'>Carica un'immagine del grappolo per valutarne la maturazione.</div>", unsafe_allow_html=True)
